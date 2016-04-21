de5a9d031d5b28b14e51c56fe821b45ea9efe9e5
Antony Raj <antony.java@gmail.com>
2016-04-21 19:18:33 +0530

    Using Task instead of async from httpotion

--- web/controllers/benchmark_controller.ex
+++ web/controllers/benchmark_controller.ex
@@ -4,9 +4,9 @@ defmodule PhoenixBenchmark.BenchmarkController do
   use PhoenixBenchmark.Web, :controller
 
   def index(conn, _params) do
-    async_output = ~w(detectors app_search search_inside)
-      |> Enum.map(&make_calls(&1))
-      |> Enum.map(&register_callback(&1))
+    async_output = [&detectors/0, &app_search/0, &search_inside/0]
+      |> Enum.map(&Task.async(&1))
+      |> Enum.map(&Task.await(&1))
       |> Enum.map(&Poison.decode(&1))
       |> Enum.map(&get_response_time(&1))
       |> List.flatten
@@ -16,6 +16,18 @@ defmodule PhoenixBenchmark.BenchmarkController do
       |> Enum.map(&sync_call_parser(&1))
       |> List.flatten
     json conn, async_output ++ sync_out
+  end
+
+  defp detectors do
+    make_sync_call("detectors")
+  end
+
+  defp app_search do
+    make_sync_call("app_search")
+  end
+
+  defp search_inside do
+    make_sync_call("search_inside")
   end
 
   defp make_sync_call(path) do
@@ -43,20 +55,5 @@ defmodule PhoenixBenchmark.BenchmarkController do
 
   defp parse_search_inside_json(response) do
     Map.get(response, "searchInsideResults") |> List.first |> Map.get("hits") |> List.first |> Map.get("score")
-  end
-
-  defp make_calls(path) do
-    %HTTPotion.AsyncResponse{id: _pid} = HTTPotion.get "#{Dotenv.get("API_SERVER")}/#{path}", [stream_to: self]
-  end
-
-  defp register_callback(pid, response \\ "") do
-    receive do
-      %HTTPotion.AsyncChunk{chunk: chunk, id: pid} -> register_callback(pid, response <> chunk)
-      %HTTPotion.AsyncEnd{id: _pid} -> 
-        if response == "" do
-          IEx.pry
-        end
-        response
-    end
   end
 end
--- web/models/detectors.ex
+++ web/models/detectors.ex
@@ -1,7 +1,27 @@
 defmodule Detectors do
   use HTTPotion.Base
+  @response ""
 
   def process_url(url) do
     "#{Dotenv.get("API_SERVER")}/#{url}"
+  end
+
+  def process1() do
+    %HTTPotion.AsyncResponse{id: pid} = get 'detectors', stream_to: self
+    register_callback(pid)
+  end
+
+  def body do
+    @response
+  end
+
+  defp register_callback(pid) do
+    receive do
+      %HTTPotion.AsyncChunk{chunk: chunk, id: pid} -> 
+        @response = @response <> chunk
+        register_callback(pid)
+      %HTTPotion.AsyncEnd{id: _pid} -> 
+        @response
+    end
   end
 end
